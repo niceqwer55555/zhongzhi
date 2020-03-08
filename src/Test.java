51d0b72fdeffbbae1b85d6f34fcd014d7c3b8a28
xu1991 <347921622@qq.com>
2020-03-08 16:43:21 +0800

    X的平方根

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+import  java.io.*;
+import java.lang.*;
+import java.nio.Buffer;
+
+//3 试用单分支结构设计一设计一判断用户输入值 x，当x大于零时求x值平方根，否则不执行任何操作的程序。
+//public class Test {
+//    public static void main(String[] args) throws IOException {
+//        float x;
+//        InputStreamReader reader = new InputStreamReader(System.in);
+//        BufferedReader input = new BufferedReader(reader);
+//        System.out.println("请输入 x:");
+//        String temp = input.readLine();
+//        x = Float.parseFloat(temp); //字符串转换为单精度浮点型
+//        if (x > 0)
+//            System.out.println(" 平方根值 " + Math.sqrt(x));
+//    }
+//}
+
